e3593f730e1b6229aa9cdfba7f143be222ac92ed
Vincent DURAND <wyllen44@gmail.com>
2012-10-22 16:05:33 +0200

    wp-config de base

--- wp-config.php
+++ wp-config.php
@@ -19,16 +19,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'g4groupe1_agea');
+define('DB_NAME', 'agea');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'g4groupe1_test');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'agea2012');
+define('DB_PASSWORD', '');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', 'mysql2.alwaysdata.com');
+define('DB_HOST', 'localhost');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8');
