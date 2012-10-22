05044880e5b47e7431f76cc9267cd3a7887d784f
Antoine Guillien <sladix@gmail.com>
2012-10-22 09:07:39 +0200

    Début de configuration de la fiche candidat

--- wp-content/themes/agea/functions.php
+++ wp-content/themes/agea/functions.php
@@ -1,11 +1,15 @@
 <?php
 	date_default_timezone_set("Europe/Paris");
 
+
+	//On crée et enregistre le post type
 	register_post_type('fiche', array(
 		'label'             => 'Fiche Candidat',
-		'add_new_item'      =>'Ajouter une fiche',
+		'labels' => array(
+				'add_new_item'      =>'Ajouter une fiche candidat'
+			),
 		'edit_item'         =>'Editer une fiche',
-		'new_item'          =>'Ajouter un picto',
+		'new_item'          =>'Ajouter une fiche',
 		'view_item'         =>'Consulter une fiche',
 		'not_found'         =>'Fiche non trouvée',
 		'menu_name'         =>'Fiches Candidats',
@@ -21,6 +25,24 @@
 		'show_in_nav_menus' => true
 	)	
 	);
+
+	//
+	//Puis la taxonomy qui sera utilisée pour les fiches
+	register_taxonomy( "mobilite_géographique", array( "fiche" ), array(
+		"hierarchical" 		=> true,
+		"label" 			=> "Mobilité géographique",
+		"labels"			=> array(
+			"name" => "Mobilité géographique",
+			"search_name" => "Rechercher par mobilité géographique",
+			"singular_name" => "Département",
+			"add_new_item" => "Ajouter un département",
+			"choose_from_most_used" => null,
+			"parent_item" => "Ce select ne sert à rien et il ne faut pas l'utiliser =D",
+			"most_used" => null,
+			"separate_items_with_commas" => "Séparez les départements par des virgules"),
+		"rewrite" 			=> true, "slug" => 'departements' )
+	);
+	
 	
 		register_post_type('news', array(
 		'label'             => 'A la une',
